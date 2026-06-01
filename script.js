c9952e9d4af18899845204fba0573bdda249a70c
Hygor <hygorpms@gmail.com>
2026-06-01 18:26:46 -0300

    comando para shiny

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function buscarPokemon(){
+/*async function buscarPokemon(){
 
     const pokemon =
         document.getElementById('pokemonInput').value
@@ -13,7 +13,52 @@ async function buscarPokemon(){
 
     document.getElementById('nome').innerText =
         dados.name
+        
 
     document.getElementById('imagem').src =
         dados.sprites.front_default
+    
+    document.getElementById('imagemShiny').src =
+        dados.sprites.front_shiny
+    
+}   
+
+*/
+
+
+async function buscarPokemon() {
+
+    let pokemon =
+        document.getElementById('pokemonInput').value.toLowerCase()
+
+    const shiny =
+        pokemon.startsWith("shiny ")
+
+    if (shiny) {
+        pokemon = pokemon.replace("shiny ", "")
+    }
+
+    const resposta =
+        await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        )
+
+    const dados =
+        await resposta.json()
+
+    document.getElementById('nome').innerText =
+        dados.name
+
+    if (shiny) {
+
+        document.getElementById('imagem').src =
+            dados.sprites.front_shiny
+
+    } else {
+
+        document.getElementById('imagem').src =
+            dados.sprites.front_default
+
+    }
+
 }
